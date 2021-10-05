70bb41d3f0a0020cf938931f00c307667aa71790
karkuzhalibalasubramanian <91403804+karkuzhalibalasubramanian@users.noreply.github.com>
2021-10-05 20:16:38 +0530

    Add files via upload

---
+++ Arraynew.java
@@ -0,0 +1,26 @@
+Package Collecetion;
+
+import  java.util.ArrayList;
+
+
+class Arraynew
+ {
+ 
+	public static void main(String[] args) 
+	{  
+
+		ArrayList<Integer> numb=new ArrayList<Integer>();
+		numb.add(1);
+        numb.add(2);
+		numb.add(3);
+		numb.add(4);
+		numb.add(5);
+		numb.add(6);
+		numb.add(7);
+		numb.add(8);
+		numb.add(9);
+		numb.add(10);
+		System.out.println(numb);
+	}
+}
+ 
