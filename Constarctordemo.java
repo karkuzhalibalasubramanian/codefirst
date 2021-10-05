f27cdbf0660b5b7d226d7afc86570f0f92c7a6e1
karkuzhalibalasubramanian <91403804+karkuzhalibalasubramanian@users.noreply.github.com>
2021-10-05 20:17:34 +0530

    Add files via upload

---
+++ Constarctordemo.java
@@ -0,0 +1,21 @@
+class Constractordemo 
+{
+	Constractordemo()
+	{
+		String name;
+		System.out.prinln("unknown");}
+    Constracordemo(String name)
+		{
+		this.name=name;
+		System.out.println(name);
+	}
+
+public static void main(string[] args)
+	{
+	Constractordemo ch1=new Constractordemo ("karkuzhali");
+	Constractordemo ch2=new Consrtactoedemo ();
+   
+		 
+		
+	}
+}
